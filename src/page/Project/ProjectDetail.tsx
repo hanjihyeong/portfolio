4ad1270eac7de3a4cc69a5838a82a8b4541f32b1
Jihyeong Han <hjh000504@naver.com>
2024-01-16 17:08:51 +0900

    ReDesign About, ProjectDetail

--- src/page/Project/ProjectDetail.tsx
+++ src/page/Project/ProjectDetail.tsx
@@ -35,77 +35,13 @@ const ProjectDetail = () => {
   }
   return (
     <StContainer>
-      {/* <StProjectSection>
-        <StButton onClick={() => navigate("/projects")}>돌아가기</StButton>
-        <StProjectInfo>
-          <StProjectImg src={project.image} alt="projectImg" />
-          <StProjectInfoSection>
-            <StProjectTitle>{project.title}</StProjectTitle>
-            <StProjectDate>기간 : {project.date}</StProjectDate>
-            <StProjectDesc>{project.desc}</StProjectDesc>
-            <StProjectLinkSection>
-              <strong>GitHub</strong>
-              <StProjectLink href={project.link}>{project.link}</StProjectLink>
-            </StProjectLinkSection>
-          </StProjectInfoSection>
-        </StProjectInfo>
-        <StBottomSection>
-          <StProjectRoleSection>
-            <StLabel>프로젝트 개요</StLabel>
-            <StUlSection>
-              {Object.entries(project.info).map(([key, value], index) => (
-                <StLiSection key={index}>
-                  <strong>{key}</strong> : {value}
-                </StLiSection>
-              ))}
-            </StUlSection>
-          </StProjectRoleSection>
-          <StProjectRoleSection>
-            <StLabel>프로젝트 주요 기능</StLabel>
-            <StUlSection>
-              {Object.entries(project.function).map(([key, value], index) => (
-                <StLiSection key={index}>
-                  <strong>{key}</strong> : {value}
-                </StLiSection>
-              ))}
-            </StUlSection>
-          </StProjectRoleSection>
-          <StProjectRoleSection>
-            <StLabel>맡은 역할</StLabel>
-            <StUlSection>
-              {project.role.map((role, index) => (
-                <StLiSection key={index}>{role}</StLiSection>
-              ))}
-            </StUlSection>
-          </StProjectRoleSection>
-          <StProjectRoleSection>
-            <StLabel>사용 기술</StLabel>
-            <StUlSection>
-              {Object.entries(project.tech).map(([key, value], index) => (
-                <StLiSection key={index}>
-                  <strong>{key}</strong>: {value}
-                </StLiSection>
-              ))}
-            </StUlSection>
-          </StProjectRoleSection>
-          <StProjectRoleSection>
-            <StLabel>회고</StLabel>
-            <StUlSection>
-              <StLiSection>
-                <strong>잘된 점</strong> : {project.memoir[0]}
-              </StLiSection>
-              <StLiSection>
-                <strong>잘못된 점</strong> : {project.memoir[1]}
-              </StLiSection>
-              <StLiSection>
-                <strong>앞으로</strong> : {project.memoir[2]}
-              </StLiSection>
-            </StUlSection>
-          </StProjectRoleSection>
-        </StBottomSection>
-      </StProjectSection> */}
       <StLeftSection>
         <StButton onClick={() => navigate("/projects")}>돌아가기</StButton>
+
+        <StProjectImage src={project.image} />
+        <h1>{project.title}</h1>
+      </StLeftSection>
+      <StRightSection>
         <StArticlesSection>
           <StArticles>
             <StArticlesTitle>개발 기간</StArticlesTitle>
@@ -161,10 +97,6 @@ const ProjectDetail = () => {
             </StLiSection>
           </StProjectInfo>
         </StProjectDetail>
-      </StLeftSection>
-      <StRightSection>
-        <StProjectImage src={project.image} />
-        <h1>{project.title}</h1>
       </StRightSection>
     </StContainer>
   );
@@ -184,11 +116,14 @@ const StContainer = styled.main`
 `;
 
 const StLeftSection = styled.section`
-  width: 60%;
+  width: 39%;
   height: 100vh;
-  background-color: white;
+  background-color: #6e787d;
   border-radius: 30px;
-  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 `;
 
 const StArticlesSection = styled.div`
@@ -249,14 +184,11 @@ const StLabel = styled.label`
 `;
 
 const StRightSection = styled.section`
-  width: 39%;
+  width: 60%;
   height: 100vh;
-  background-color: #6e787d;
+  background-color: white;
   border-radius: 30px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
+  position: relative;
 `;
 
 const StProjectImage = styled.img`
@@ -277,116 +209,6 @@ const StButton = styled.button`
     border: none;
   }
 `;
-
-// const StBottomSection = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   gap: 10px;
-//   overflow-y: scroll;
-// &::-webkit-scrollbar {
-//   width: 10px;
-// }
-
-// &::-webkit-scrollbar-thumb {
-//   background-color: #6e787d;
-//   border-radius: 10px;
-// }
-
-// &::-webkit-scrollbar-track {
-//   background-color: none;
-// }
-// `;
-
-// const StProjectSection = styled.section`
-//   display: flex;
-//   gap: 10px;
-//   width: 55%;
-//   height: 100vh;
-//   align-items: flex-start;
-//   justify-content: flex-start;
-//   border: 2px solid lightgray;
-//   background-color: white;
-//   flex-direction: column;
-//   position: relative;
-// `;
-
-// const StProjectInfo = styled.div`
-//   display: flex;
-//   border-bottom: 2px solid lightgray;
-//   padding-bottom: 10px;
-//   width: 100%;
-// `;
-
-// const StProjectInfoSection = styled.section`
-//   margin-left: 15px;
-//   width: 70%;
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: flex-start;
-//   align-items: flex-start;
-// `;
-
-// const StProjectImg = styled.img`
-//   width: 180px;
-//   height: 180px;
-//   border-radius: 30px;
-//   overflow: hidden;
-//   margin-left: 20px;
-//   margin-top: 20px;
-//   @media (max-width: 1000px) {
-//     display: none;
-//   }
-// `;
-
-// const StProjectTitle = styled.h1`
-//   font-size: 2rem;
-//   margin-bottom: 0;
-// `;
-
-// const StProjectDate = styled.p`
-//   margin: 15px 0 0 0;
-//   font-weight: bold;
-// `;
-
-// const StProjectDesc = styled.p`
-//   width: 90%;
-//   margin: 10px 0 0 0;
-//   font-weight: bold;
-// `;
-
-// const StProjectLinkSection = styled.div`
-//   display: flex;
-//   gap: 10px;
-//   margin: 10px 0 0 0;
-//   width: 100%;
-//   overflow: hidden;
-// `;
-
-// const StProjectLink = styled.a`
-//   width: 90%;
-//   overflow: hidden;
-//   color: black;
-//   font-weight: bold;
-//   &:hover {
-//     color: black;
-//   }
-// `;
-
-// const StProjectRoleSection = styled.section`
-//   display: flex;
-//   flex-direction: column;
-//   margin-left: 15px;
-// `;
-
-// const StLabel = styled.label`
-//   font-size: 1.2rem;
-//   font-weight: bold;
-//   margin-bottom: 10px;
-// `;
-
-// const StUlSection = styled.ul`
-//   margin: 0 0 10px 0;
-// `;
 
 const StLiSection = styled.li`
   margin-bottom: 10px;
